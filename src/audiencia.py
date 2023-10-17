76c507af0131cbf99748f926b855a3c52171c087
dlozaco975@gmail.com <dlozaco975@gmail.com>
2023-10-17 22:50:34 +0200

    agrupa y media por ediciones

--- src/audiencia.py
+++ src/audiencia.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 Audiencia = namedtuple('Audiencia', 'edicion, share')
 
-def leer_audiencia(archivo)->list[Audiencia]:
+def leer_audiencia(archivo:str)->list[Audiencia]:
     audiencia = list()
 
     with open(archivo, 'rt', encoding='utf-8') as f:
@@ -15,7 +15,7 @@ def leer_audiencia(archivo)->list[Audiencia]:
     
     return audiencia
 
-def calcula_ediciones(archivo:list[Audiencia])->int:
+def calcula_ediciones(archivo:str)->int:
     
     ediciones = set()
     for edicion, share in archivo:
@@ -23,12 +23,28 @@ def calcula_ediciones(archivo:list[Audiencia])->int:
     
     return len(ediciones)
 
-def filtrar_ediciones(archivo:list[Audiencia], e:int)->list:
-    e_list = list()
+def filtrar_ediciones(archivo:str, e:int)->list:#e de edicion
+    res = list()
     for edicion, share in archivo:
         if edicion in e:
-            e_list.append((int(edicion), float(share)))
+            res.append((int(edicion), float(share)))
 
-    return e_list
+    return res
+
+def agrupa_por_ediciones(archivo:str):
+    res = dict()
+    for edicion, share in archivo:
+        if edicion in res:
+            res[edicion].append(share)
+        else:
+            res[edicion] = [share]
+    return res
+
+def media_por_ediciones(archivo:str)->list[Audiencia]:
+    medias = dict()
+    lista = agrupa_por_ediciones(archivo)
+    for edicion, lista_shares in lista.items():
+        medias[edicion] = sum(lista_shares)/len(lista_shares)
+    return medias
 
 
--- src/test_audencia.py
+++ src/test_audencia.py
@@ -30,17 +30,41 @@ def test_filtrar_ediciones_mc(audiencia:list[Audiencia]):
     print("\nAudiencias de las tres primeras ediciones del programa MasterChef:")
     print(audiencias_mc_123)
 
+def test_agrupa_por_ediciones_gh(edicion:dict):
+    d_gh = agrupa_por_ediciones(edicion)
+    print("Agrupación por ediciones de las audiencias del programa Gran Hermano:")
+    print(d_gh)
+
+def test_agrupa_por_ediciones_mc(edicion:dict):
+    d_gh = agrupa_por_ediciones(edicion)
+    print("\nAgrupación por ediciones de las audiencias del programa MasterChef:")
+    print(d_gh)
+
+def test_media_por_ediciones_gh(archivo:list[Audiencia]):
+    media_gh = media_por_ediciones(archivo)
+    print("Media de audiencias por ediciones del programa Gran Hermano:")
+    print(media_gh)
+
+def test_media_por_ediciones_mc(archivo:list[Audiencia]):
+    media_mc = media_por_ediciones(archivo)
+    print("\nMedia de audiencias por ediciones del programa MasterChef:")
+    print(media_mc)
+
 if __name__ == '__main__':
 
     audiencias_gh = leer_audiencia('./data/GH.csv')
     audiencias_mc = leer_audiencia('./data/MasterChef.csv')
-
+    
     # test_leer_audencia_gh(audiencias_gh)
     # test_leer_audencia_mc(audiencias_mc)
     # test_calcula_ediciones_gh(audiencias_gh)
     # test_calcula_ediciones_mc(audiencias_mc)
-    test_filtrar_ediciones_gh(audiencias_gh)
-    test_filtrar_ediciones_mc(audiencias_mc)
+    # test_filtrar_ediciones_gh(audiencias_gh)
+    # test_filtrar_ediciones_mc(audiencias_mc)
+    # test_agrupa_por_ediciones_gh(audiencias_gh)
+    # test_agrupa_por_ediciones_mc(audiencias_mc)
+    test_media_por_ediciones_gh(audiencias_gh)
+    test_media_por_ediciones_mc(audiencias_mc)
 
 
 
